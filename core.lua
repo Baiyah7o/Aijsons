b47726a9bbfedec3850896507e3ea093957e1dea
Baiyah7o <Baiyah7o@localdomain>
2022-11-27 08:27:54 +0100

    updated method stats ramking fot 10.0

--- core.lua
+++ core.lua
@@ -60,106 +60,115 @@ local function GetStatsDistrib()
         MyStatsText =  MyStatsText .. '\nItem level increases intellect and stamina. When at the same item level the priority should be:\n\n'
         MyStatsText =  MyStatsText .. "Intellect (" .. UnitStat("player",4)                       .. ") > "
         MyStatsText =  MyStatsText .. "Haste ("     .. GetCombatRating(CR_HASTE_SPELL)             .. ") > "
-        MyStatsText =  MyStatsText .. "Mastery ("   .. GetCombatRating(CR_MASTERY)                 .. ") => "
         MyStatsText =  MyStatsText .. "Crit ("      .. GetCombatRating(CR_CRIT_SPELL)              .. ") > "
+        MyStatsText =  MyStatsText .. "Mastery ("   .. GetCombatRating(CR_MASTERY)                 .. ") > "
         MyStatsText =  MyStatsText .. "Vers ("      .. GetCombatRating(CR_VERSATILITY_DAMAGE_DONE) .. ")\n"
         MyStatsText =  MyStatsText .. "\nAbove stats for " .. class .. " shown ordered according to:\n" 
-        MyStatsText =  MyStatsText .. " - https://www.wowhead.com/guide/classes/warlock/destruction/stat-priority-pve-dps\n" 
-        MyStatsText =  MyStatsText .. " - https://www.method.gg/guides/destruction-warlock/stats-races-and-consumables\n" 
+        MyStatsText =  MyStatsText .. " https://www.wowhead.com/guide/classes/warlock/destruction/stat-priority-pve-dps\n" 
+        MyStatsText =  MyStatsText .. " https://www.method.gg/guides/destruction-warlock/stats-races-and-consumables\n" 
     elseif id == 70 then
         -- Retribution Paladin, Mastery > Haste = Versatility > Crit
         MyStatsText =  MyStatsText .. class .. ' (Retribution Paladin, ID ' .. id ..')\n'
         MyStatsText =  MyStatsText .. "Strength ("  .. UnitStat("player",1)                        .. ") > "
-        MyStatsText =  MyStatsText .. "Mastery ("   .. GetCombatRating(CR_MASTERY)                 .. ") > "
         MyStatsText =  MyStatsText .. "Haste ("     .. GetCombatRating(CR_HASTE_SPELL)             .. ") = "
+        MyStatsText =  MyStatsText .. "Mastery ("   .. GetCombatRating(CR_MASTERY)                 .. ") = "
         MyStatsText =  MyStatsText .. "Vers ("      .. GetCombatRating(CR_VERSATILITY_DAMAGE_DONE) .. ") > "
         MyStatsText =  MyStatsText .. "Crit ("      .. GetCombatRating(CR_CRIT_SPELL)              .. ")\n "
         MyStatsText =  MyStatsText .. "\nAbove stats for " .. class .. " shown ordered according to:\n" 
-        MyStatsText =  MyStatsText .. " - https://www.wowhead.com/guide/classes/paladin/retribution/stat-priority-pve-dps\n" 
-        MyStatsText =  MyStatsText .. " - https://www.method.gg/guides/retribution-paladin/stats-races-and-consumables\n" 
+        MyStatsText =  MyStatsText .. " https://www.wowhead.com/guide/classes/paladin/retribution/stat-priority-pve-dps\n" 
+        MyStatsText =  MyStatsText .. " https://www.method.gg/guides/retribution-paladin/stats-races-and-consumables\n" 
     elseif id == 104 then
         -- Guardian Druid, Versatility > Mastery >= Haste > Crit
         MyStatsText =  MyStatsText .. class .. ' (Guardian Druid, ID ' .. id ..')\n'
+        MyStatsText =  MyStatsText .. '\nWhen comparing items of similar ilvl, you can use the following stat priority for defense:\n\n'
         MyStatsText =  MyStatsText .. "Agility ("   .. UnitStat("player",2)                        .. ") > "
         MyStatsText =  MyStatsText .. "Vers ("      .. GetCombatRating(CR_VERSATILITY_DAMAGE_DONE) .. ") > "
-        MyStatsText =  MyStatsText .. "Mastery ("   .. GetCombatRating(CR_MASTERY)                 .. ") ≥ "
         MyStatsText =  MyStatsText .. "Haste ("     .. GetCombatRating(CR_HASTE_SPELL)             .. ") > "
-        MyStatsText =  MyStatsText .. "Crit ("      .. GetCombatRating(CR_CRIT_SPELL)              .. ")\n "
+        MyStatsText =  MyStatsText .. "Crit ("      .. GetCombatRating(CR_CRIT_SPELL)              .. ") > "
+        MyStatsText =  MyStatsText .. "Mastery ("   .. GetCombatRating(CR_MASTERY)                 .. ")\n "
         MyStatsText =  MyStatsText .. "\nAbove stats for " .. class .. " shown ordered according to:\n" 
-        MyStatsText =  MyStatsText .. " - https://www.wowhead.com/guide/classes/druid/guardian/stat-priority-pve-tank\n" 
-        MyStatsText =  MyStatsText .. " - https://www.method.gg/guides/guardian-druid/stats-races-and-consumables\n" 
+        MyStatsText =  MyStatsText .. " https://www.wowhead.com/guide/classes/druid/guardian/stat-priority-pve-tank\n" 
+        MyStatsText =  MyStatsText .. " https://www.method.gg/guides/guardian-druid/stats-races-and-consumables\n" 
     elseif id == 105 then
         -- Restoration Druid, Versatility > Mastery >= Haste > Crit
         MyStatsText =  MyStatsText .. class .. ' (Restoration Druid, for raiding:, ID ' .. id ..')\n'
         MyStatsText =  MyStatsText .. "Intellect (" .. UnitStat("player",4)                        .. ") = "
         MyStatsText =  MyStatsText .. "Haste ("     .. GetCombatRating(CR_HASTE_SPELL)             .. ") > "
-        MyStatsText =  MyStatsText .. "Crit ("      .. GetCombatRating(CR_CRIT_SPELL)              .. ") = "
-        MyStatsText =  MyStatsText .. "Vers ("      .. GetCombatRating(CR_VERSATILITY_DAMAGE_DONE) .. ") = "
-        MyStatsText =  MyStatsText .. "Mastery ("   .. GetCombatRating(CR_MASTERY)                 .. ") \n\n"
+        MyStatsText =  MyStatsText .. "Mastery ("   .. GetCombatRating(CR_MASTERY)                 .. ") > "
+        MyStatsText =  MyStatsText .. "Crit ("      .. GetCombatRating(CR_CRIT_SPELL)              .. ") >= "
+        MyStatsText =  MyStatsText .. "Vers ("      .. GetCombatRating(CR_VERSATILITY_DAMAGE_DONE) .. ") \n\n "
+        
         MyStatsText =  MyStatsText .. class .. ' (Restoration Druid, for m+:, ID ' .. id ..')\n'
         MyStatsText =  MyStatsText .. "Intellect (" .. UnitStat("player",4)                        .. ") > "
-        MyStatsText =  MyStatsText .. "Mastery ("   .. GetCombatRating(CR_MASTERY)                 .. ") > "
         MyStatsText =  MyStatsText .. "Haste ("     .. GetCombatRating(CR_HASTE_SPELL)             .. ") > "
+        MyStatsText =  MyStatsText .. "Vers ("      .. GetCombatRating(CR_VERSATILITY_DAMAGE_DONE) .. ") >="
         MyStatsText =  MyStatsText .. "Crit ("      .. GetCombatRating(CR_CRIT_SPELL)              .. ") > "
-        MyStatsText =  MyStatsText .. "Vers ("      .. GetCombatRating(CR_VERSATILITY_DAMAGE_DONE) .. ") \n"
+        MyStatsText =  MyStatsText .. "Mastery ("   .. GetCombatRating(CR_MASTERY)                 .. ") \n "
         MyStatsText =  MyStatsText .. "\nAbove stats for " .. class .. " shown ordered according to:\n" 
-        MyStatsText =  MyStatsText .. " - https://www.wowhead.com/guide/classes/druid/restoration/stat-priority-pve-healer\n" 
-        MyStatsText =  MyStatsText .. " - https://www.method.gg/guides/restoration-druid/stats-races-and-consumables\n" 
+        MyStatsText =  MyStatsText .. " https://www.wowhead.com/guide/classes/druid/restoration/stat-priority-pve-healer\n" 
+        MyStatsText =  MyStatsText .. " https://www.method.gg/guides/restoration-druid/stats-races-and-consumables\n" 
     elseif id == 250 then
         -- Death Knight - Blood, Haste > Versatility > Critical Strike > Mastery
-        MyStatsText =  MyStatsText .. class .. ' (Blood Death Knight, ID ' .. id ..')\n'
+        MyStatsText =  MyStatsText .. class .. ' (Blood Death Knight, ID ' .. id ..') (Version 9.2.5)\n'
         MyStatsText =  MyStatsText .. '\nItem level over all stats as more stamina and main stat always are good. When at the same item level the priority should be:\n\n'
         MyStatsText =  MyStatsText .. "Haste ("     .. GetCombatRating(CR_HASTE_SPELL)             .. ") > "
         MyStatsText =  MyStatsText .. "Vers ("      .. GetCombatRating(CR_VERSATILITY_DAMAGE_DONE) .. ") > "
         MyStatsText =  MyStatsText .. "Crit ("      .. GetCombatRating(CR_CRIT_SPELL)              .. ") > "
         MyStatsText =  MyStatsText .. "Mastery ("   .. GetCombatRating(CR_MASTERY)                 .. ")\n "
         MyStatsText =  MyStatsText .. "\nAbove stats for " .. class .. " shown ordered according to:\n" 
-        MyStatsText =  MyStatsText .. " - https://www.wowhead.com/guide/classes/death-knight/blood/stat-priority-pve-tank\n" 
-        MyStatsText =  MyStatsText .. " - https://www.method.gg/guides/blood-death-knight/stats-races-and-consumables\n" 
+        MyStatsText =  MyStatsText .. " https://www.wowhead.com/guide/classes/death-knight/blood/stat-priority-pve-tank\n" 
+        MyStatsText =  MyStatsText .. " https://www.method.gg/guides/blood-death-knight/stats-races-and-consumables\n" 
     elseif id == 253 then
         -- Beats Mastery Hunter, Critical Strike > Haste > Versatility > Mastery
         MyStatsText =  MyStatsText .. class .. ' (Beats Mastery Hunter, ID ' .. id ..')\n'
+        MyStatsText =  MyStatsText .. "\nSingle Target Stat Priority:\n\n"
         MyStatsText =  MyStatsText .. "Agility ("   .. UnitStat("player",2)                        .. ") > "
         MyStatsText =  MyStatsText .. "Crit ("      .. GetCombatRating(CR_CRIT_SPELL)              .. ") > "
-        MyStatsText =  MyStatsText .. "Haste ("     .. GetCombatRating(CR_HASTE_SPELL)             .. ") > "
         MyStatsText =  MyStatsText .. "Vers ("      .. GetCombatRating(CR_VERSATILITY_DAMAGE_DONE) .. ") > "
+        MyStatsText =  MyStatsText .. "Haste ("     .. GetCombatRating(CR_HASTE_SPELL)             .. ") > "
         MyStatsText =  MyStatsText .. "Mastery ("   .. GetCombatRating(CR_MASTERY)                 .. ")\n "
+        MyStatsText =  MyStatsText .. "\nAoE Stat Priority:\n\n"
+        MyStatsText =  MyStatsText .. "Agility ("   .. UnitStat("player",2)                        .. ") > "
+        MyStatsText =  MyStatsText .. "Mastery ("   .. GetCombatRating(CR_MASTERY)                 .. ") > "
+        MyStatsText =  MyStatsText .. "Haste ("     .. GetCombatRating(CR_HASTE_SPELL)             .. ") > "
+        MyStatsText =  MyStatsText .. "Crit ("      .. GetCombatRating(CR_CRIT_SPELL)              .. ") > "
+        MyStatsText =  MyStatsText .. "Vers ("      .. GetCombatRating(CR_VERSATILITY_DAMAGE_DONE) .. ") \n "  
         MyStatsText =  MyStatsText .. "\nAbove stats for " .. class .. " shown ordered according to:\n" 
-        MyStatsText =  MyStatsText .. " - https://www.wowhead.com/guide/classes/hunter/beast-mastery/stat-priority-pve-dps\n" 
-        MyStatsText =  MyStatsText .. " - https://www.method.gg/guides/beast-mastery-hunter/stats-races-and-consumables\n" 
+        MyStatsText =  MyStatsText .. " https://www.wowhead.com/guide/classes/hunter/beast-mastery/stat-priority-pve-dps\n" 
+        MyStatsText =  MyStatsText .. " https://www.method.gg/guides/beast-mastery-hunter/stats-races-and-consumables\n" 
     elseif id == 266 then
         -- Intellect / Item Level >>> Haste > Mastery = Crit > Versatility
         MyStatsText =  MyStatsText .. class .. ' (Demonology Warlock, ID ' .. id ..')\n'
         MyStatsText =  MyStatsText .. '\nItem level increases intellect and stamina. When at the same item level the priority should be:\n\n'
-        MyStatsText =  MyStatsText .. "Intellect (" .. UnitStat("player",4)                        .. ") >> "
+        MyStatsText =  MyStatsText .. "Intellect (" .. UnitStat("player",4)                        .. ") > "
         MyStatsText =  MyStatsText .. "Haste ("     .. GetCombatRating(CR_HASTE_SPELL)             .. ") > "
         MyStatsText =  MyStatsText .. "Mastery ("   .. GetCombatRating(CR_MASTERY)                 .. ") = "
-        MyStatsText =  MyStatsText .. "Crit ("      .. GetCombatRating(CR_CRIT_SPELL)              .. ") > "
         MyStatsText =  MyStatsText .. "Vers ("      .. GetCombatRating(CR_VERSATILITY_DAMAGE_DONE) .. ")\n "
+        MyStatsText =  MyStatsText .. "Crit ("      .. GetCombatRating(CR_CRIT_SPELL)              .. ") is irrelevant\n "
         MyStatsText =  MyStatsText .. "\nAbove stats for " .. class .. " shown ordered according to:\n" 
-        MyStatsText =  MyStatsText .. " - https://www.wowhead.com/guide/classes/warlock/demonology/stat-priority-pve-dps\n" 
-        MyStatsText =  MyStatsText .. " - https://www.method.gg/guides/demonology-warlock/stats-races-and-consumables\n" 
+        MyStatsText =  MyStatsText .. " https://www.wowhead.com/guide/classes/warlock/demonology/stat-priority-pve-dps\n" 
+        MyStatsText =  MyStatsText .. " https://www.method.gg/guides/demonology-warlock/stats-races-and-consumables\n" 
     elseif id == 1468 then
         -- Intellect / Item Level >>> Haste > Mastery = Crit > Versatility
         MyStatsText =  MyStatsText .. class .. ' (Preservation Evoker, ID ' .. id ..')\n'
         MyStatsText =  MyStatsText .. '\nItem level increases intellect and stamina. When at the same item level the priority should be for raiding:\n\n'
         MyStatsText =  MyStatsText .. "Mastery ("   .. GetCombatRating(CR_MASTERY)                 .. ") > "
         MyStatsText =  MyStatsText .. "Crit ("      .. GetCombatRating(CR_CRIT_SPELL)              .. ") > "
-        MyStatsText =  MyStatsText .. "Haste ("     .. GetCombatRating(CR_HASTE_SPELL)             .. ") > "
-        MyStatsText =  MyStatsText .. "Vers ("      .. GetCombatRating(CR_VERSATILITY_DAMAGE_DONE) .. ")\n "
-        MyStatsText =  MyStatsText .. '\nPriority should be for Mythic+, healing focus:\n\n'
-        MyStatsText =  MyStatsText .. "Crit ("      .. GetCombatRating(CR_CRIT_SPELL)              .. ") = "
         MyStatsText =  MyStatsText .. "Haste ("     .. GetCombatRating(CR_HASTE_SPELL)             .. ") = "
+        MyStatsText =  MyStatsText .. "Vers ("      .. GetCombatRating(CR_VERSATILITY_DAMAGE_DONE) .. ")\n "
+        MyStatsText =  MyStatsText .. '\nMythic+:\n\n'
+        MyStatsText =  MyStatsText .. "Crit ("      .. GetCombatRating(CR_CRIT_SPELL)              .. ") > "
         MyStatsText =  MyStatsText .. "Vers ("      .. GetCombatRating(CR_VERSATILITY_DAMAGE_DONE) .. ") > "
+        MyStatsText =  MyStatsText .. "Haste ("     .. GetCombatRating(CR_HASTE_SPELL)             .. ") > "
         MyStatsText =  MyStatsText .. "Mastery ("   .. GetCombatRating(CR_MASTERY)                 .. ") \n "
         MyStatsText =  MyStatsText .. "\nAbove stats for " .. class .. " shown ordered according to:\n" 
-        MyStatsText =  MyStatsText .. " - https://www.wowhead.com/ptr/guide/classes/evoker/preservation/stat-priority-pve-healer\n" 
+        MyStatsText =  MyStatsText .. " https://www.wowhead.com/ptr/guide/classes/evoker/preservation/stat-priority-pve-healer\n" 
     else
         MyStatsText =  MyStatsText .. class .. " (not yet implemented)\n" 
     end
     -- MyStatsText =  MyStatsText .. "\nAbove stats for " .. class .. " shown ordered according to:\n" 
-    -- MyStatsText =  MyStatsText .. " - https://www.wowhead.com/classes\n" 
-    -- MyStatsText =  MyStatsText .. " - https://www.method.gg/guides)\n" 
+    -- MyStatsText =  MyStatsText .. " https://www.wowhead.com/classes\n" 
+    -- MyStatsText =  MyStatsText .. " https://www.method.gg/guides)\n" 
 
     MyStatsText =  MyStatsText .. "\nGeneral stats:\n"
     MyStatsText =  MyStatsText .. "  Strength ("       .. UnitStat("player",1)   .. ") \n"
